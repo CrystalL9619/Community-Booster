41f45b3fcf8e358c3834cfbe4b617ad109cab3ef
CrystalL9619 <145159731+CrystalL9619@users.noreply.github.com>
2024-06-16 23:57:25 -0400

    Styling

---
+++ MyPassionProject/Example.cs
@@ -0,0 +1,30 @@
+﻿// using SendGrid's C# Library
+// https://github.com/sendgrid/sendgrid-csharp
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    internal class Example
+    {
+        private static void Main()
+        {
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("CommunityBoosterEmail");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("luoetrte@gator2122.hostgator.com", "Crystal");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("liuhanzecrystal@gmail.com", "Crystal");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
